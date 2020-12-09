f761f3deff1fc779135c35111e3458444bb65b3e
ah2260 <47584058+ah2260@users.noreply.github.com>
2020-12-09 03:19:36 -0500

    Add files via upload

--- webdev final/js/main.js
+++ webdev final/js/main.js
@@ -1,19 +1,25 @@
 // JavaScript Document
+
+//initializes on page load 
 $(document).ready(function () {
 	
 	$("body").append($("<div>", { "class": "scrolltop" }).add($("<div>", { "class": "marginTop"})).hide());
 	
 	$("h1,.banner-contents p").addClass("hidden4");
+	// hides h1 and p on banner and img on sec2 initially
 	$(".layers").addClass("hidden4");
+	// hides items on services section initially
 	$(".services-lists li").addClass("hidden2");
+	// hides items on team section initially
 	$(".team-details").addClass("hidden3");
-	
+	//toggle links appear under hamburger menu
 	$(".menu-icon").click(function () {
         $(".collapse").toggle(function () {
 			$(this).addClass("block")
 		});
     });
 	
+	//after 200 ms adds fadeIn2 class to h1 and banner paragraph
 	setTimeout(function () {
         $("h1").addClass("fadeIn2");
     }, 200);
@@ -33,8 +39,9 @@ $(document).ready(function () {
 	var $banner = $('#banner');
 	var $margin = $('.margin');
 	
+	//functions on scroll
     $window.scroll(function () {
-		
+			//mathematical formulas for transitions to start depending on scroll position
             var winScrollTop = $window.scrollTop();
             var bottom_of_object1 = $firstServiceLi.offset().top + $servicesLi.outerHeight();
 			var bottom_of_object2 = $firstTeamDetail.offset().top + $teamDetails.outerHeight() / 2;
@@ -42,15 +49,17 @@ $(document).ready(function () {
             var bottom_of_window = winScrollTop + $window.height();
 			var parallax = scrollTop - winScrollTop * .75;
 			var parallax2 = marginTop + winScrollTop * .65;
-			
+			//parallex scrolling
 			$banner.css('height', parallax);
 			$margin.css('margin-top', parallax2);
 			
+			//getting scroll positions on section 2
 			if(bottom_of_window > bottom_of_object3){
-				
+				//makes image in section 2 visible
 				$layers.addClass("fadeIn2");
 				
 				setTimeout(function() {
+					//animates first layer of image on section 2
 					$(".layer1").addClass("animate1");
 					$(".layer2").addClass("animate2");
 					$(".layer3").addClass("animate3");
@@ -58,6 +67,9 @@ $(document).ready(function () {
 			}
 			
 			if(bottom_of_window > bottom_of_object1){
+				//fades items in services section 
+				//fades in items one at a time 200 ms one after the other
+				//elements transition down because they move from a negative to 0 margin
 					$servicesLi.each(function (i, item) {
                     	setTimeout(function () {
         					$(item).addClass("fadeIn");
@@ -65,7 +77,10 @@ $(document).ready(function () {
         			});		
 			}
 			
-			if(bottom_of_window > bottom_of_object2){		
+			if(bottom_of_window > bottom_of_object2){
+				//fades items in our team section 
+				//fades in items one at a time 200 ms one after the other
+				//elements transition up because they move from a positive to 0 margin
 				$teamDetails.each(function (i, item) {
                     	setTimeout(function () {
         					$(item).addClass("fadeIn");
